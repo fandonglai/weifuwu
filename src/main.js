bf32645402649f6c41ab3c4d90f92c0567454c23
fandonglai <704072802@qq.com>
2020-12-28 15:49:44 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
 
-Vue.config.productionTip = false
-
+import { registerMicroApps, start } from "qiankun";
+const apps = [
+  {
+    name: "vueApp", //应用的名字
+    entry: "//localhost:10000", //默认会加载这个html 解析里面的js 动态的执行；子应用必须支持跨域
+    container: "#vue", //容器名字
+    activeRule: "/vue", //激活的路径
+  },
+  {
+    name: "reactApp",
+    entry: "//localhost:20000", //默认会加载这个html 解析里面的js 动态的执行；子应用必须支持跨域
+    container: "#react",
+    activeRule: "/react",
+  },
+];
+registerMicroApps(apps); //注册app
+start({
+  prefetch: false, //取消预加载
+}); //开启
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
